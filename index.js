0cf058ee05b362461d32e9a28eee7e65f01d1e7f
chamikab <kasun.chamika@gmail.com>
2018-09-28 23:14:32 +0530

    Added changes for having real cluster changes.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 if (cluster.isMaster) {
   console.log('isMaster : ', cluster.isMaster);
-  cluster.fork()
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 } else {
   console.log('isMaster : ', cluster.isMaster);
   const express = require('express');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there!l');
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there!');
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
   });
 
   app.listen(3000);
