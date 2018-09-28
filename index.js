e429fab9551c71928f464e8bda7eda611179155e
chamikab <kasun.chamika@gmail.com>
2018-09-28 21:44:36 +0530

    Added Cluster Manager support.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-  res.send('Hi there!l');
-});
+if (cluster.isMaster) {
+  console.log('isMaster : ', cluster.isMaster);
+  cluster.fork()
+} else {
+  console.log('isMaster : ', cluster.isMaster);
+  const express = require('express');
+  const app = express();
 
-app.listen(3000);
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there!l');
+  });
+
+  app.listen(3000);
+}
